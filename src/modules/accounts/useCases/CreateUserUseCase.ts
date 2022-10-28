690391e6ad98ebeafed81bbc834439a7e596fdb0
Gabriel Henrique <gabriel.henrique@warren.com.br>
2022-10-28 02:13:17 -0300

    feat: encryptes password and verify if user exists

--- src/modules/accounts/useCases/CreateUserUseCase.ts
+++ src/modules/accounts/useCases/CreateUserUseCase.ts
@@ -1,6 +1,7 @@
 import { inject, injectable } from 'tsyringe';
 import { ICreateUserDTO } from '../dtos/ICreateUserDTO';
 import { IUsersRepository } from '../repositories/IUsersRepository';
+import { hash } from 'bcrypt';
 
 @injectable()
 class CreateUserUseCase {
@@ -12,10 +13,18 @@ class CreateUserUseCase {
   async execute(params: ICreateUserDTO): Promise<void> {
     const { name, email, password, driver_license } = params;
 
+    const userAlreadyExists = await this.usersRepository.findByEmail(email);
+
+    if (userAlreadyExists) {
+      throw new Error('User already exists');
+    }
+
+    const password_hash = await hash(password, 8);
+
     await this.usersRepository.create({
       name,
       email,
-      password,
+      password: password_hash,
       driver_license,
     });
   }
