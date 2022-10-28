e0b2046966c850a8603f91c8199b3061ea89238e
Gabriel Henrique <gabriel.henrique@warren.com.br>
2022-10-28 02:38:32 -0300

    refactor: inserts createuser into a folder

--- src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
@@ -1,6 +1,6 @@
 import { inject, injectable } from 'tsyringe';
-import { ICreateUserDTO } from '../dtos/ICreateUserDTO';
-import { IUsersRepository } from '../repositories/IUsersRepository';
+import { ICreateUserDTO } from '../../dtos/ICreateUserDTO';
+import { IUsersRepository } from '../../repositories/IUsersRepository';
 import { hash } from 'bcrypt';
 
 @injectable()
