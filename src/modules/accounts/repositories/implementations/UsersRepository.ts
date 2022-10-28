5f79032933f16b67d2021119a7ec2d4dec4c2399
Gabriel Henrique <gabriel.henrique@warren.com.br>
2022-10-28 02:05:11 -0300

    feat: create user

---
+++ src/modules/accounts/repositories/implementations/UsersRepository.ts
@@ -0,0 +1,26 @@
+import { getRepository, Repository } from 'typeorm';
+import { ICreateUserDTO } from '../../dtos/ICreateUserDTO';
+import { User } from '../../entities/User';
+import { IUsersRepository } from '../IUsersRepository';
+
+class UsersRepository implements IUsersRepository {
+  private repository: Repository<User>;
+
+  constructor() {
+    this.repository = getRepository(User);
+  }
+
+  async create(params: ICreateUserDTO): Promise<void> {
+    const { name, password, email, driver_license } = params;
+    const user = this.repository.create({
+      name,
+      email,
+      password,
+      driver_license,
+    });
+
+    await this.repository.save(user);
+  }
+}
+
+export { UsersRepository };
