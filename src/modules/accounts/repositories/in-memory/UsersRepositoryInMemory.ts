e3bda8af1456f650fb1900787a28ad0a94d3c5ec
Gabriel Henrique <gabriel.henrique@warren.com.br>
2022-11-06 18:09:46 -0300

    feat: create users repository in memory

---
+++ src/modules/accounts/repositories/in-memory/UsersRepositoryInMemory.ts
@@ -0,0 +1,30 @@
+import { ICreateUserDTO } from '../../dtos/ICreateUserDTO';
+import { User } from '../../entities/User';
+import { IUsersRepository } from '../IUsersRepository';
+
+class UsersRepositoryInMemory implements IUsersRepository {
+  users: User[] = [];
+
+  async create(params: ICreateUserDTO): Promise<void> {
+    const { driver_license, email, name, password } = params;
+    const user = new User();
+
+    Object.assign(user, {
+      driver_license,
+      email,
+      name,
+      password,
+    });
+
+    this.users.push(user);
+  }
+
+  async findByEmail(email: string): Promise<User> {
+    return this.users.find(user => user.email === email);
+  }
+  async findById(id: string): Promise<User> {
+    return this.users.find(user => user.id === id);
+  }
+}
+
+export { UsersRepositoryInMemory };
