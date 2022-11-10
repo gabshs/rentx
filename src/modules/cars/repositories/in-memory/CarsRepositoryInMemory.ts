4207b4fd2f8d4bb7719a51ef4df8e6febd43b6c8
Gabriel Henrique <gabriel.henrique@warren.com.br>
2022-11-09 22:34:01 -0300

    feat create car repository to test

---
+++ src/modules/cars/repositories/in-memory/CarsRepositoryInMemory.ts
@@ -0,0 +1,39 @@
+import { ICreateCarDTO } from '@modules/cars/dtos/ICreateCarDTO';
+import { Car } from '@modules/cars/infra/typeorm/entities/Car';
+import { ICarsRepository } from '@modules/cars/repositories/ICarsRepository';
+
+class CarsRepositoryInMemory implements ICarsRepository {
+  cars: Car[] = [];
+
+  async create(data: ICreateCarDTO): Promise<Car> {
+    const {
+      name,
+      description,
+      daily_rate,
+      license_plate,
+      fine_amount,
+      brand,
+      category_id,
+    } = data;
+
+    const car = new Car();
+    Object.assign(car, {
+      name,
+      daily_rate,
+      description,
+      license_plate,
+      fine_amount,
+      brand,
+      category_id,
+    });
+
+    this.cars.push(car);
+    return car;
+  }
+
+  async findByLicensePlate(license_plate: string): Promise<Car> {
+    return this.cars.find(car => car.license_plate === license_plate);
+  }
+}
+
+export { CarsRepositoryInMemory };
