2474969473a40aa80163cc4a2d38fd6016b80cd7
Gabriel Henrique <gabriel.henrique@warren.com.br>
2022-11-16 02:30:33 -0300

    feat: adds findAvailable to repository

--- src/modules/cars/repositories/ICarsRepository.ts
+++ src/modules/cars/repositories/ICarsRepository.ts
@@ -1,9 +1,15 @@
 import { ICreateCarDTO } from '@modules/cars/dtos/ICreateCarDTO';
 import { Car } from '@modules/cars/infra/typeorm/entities/Car';
 
+interface availableFilter {
+  brand?: string;
+  category_id?: string;
+  name?: string;
+}
 interface ICarsRepository {
   create(data: ICreateCarDTO): Promise<Car>;
   findByLicensePlate(license_plate: string): Promise<Car>;
+  findAvailable(filters: availableFilter): Promise<Car[]>;
 }
 
 export { ICarsRepository };
