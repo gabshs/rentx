820f6f358f482b6d4a7f18fff947f58746694c7e
Gabriel Henrique <gabriel.henrique@warren.com.br>
2022-11-28 00:32:10 -0300

    feat: adds create method / changes verify using ! operator instead of comparation with null

--- src/modules/rentals/repositories/in-memory/RentalsRepositoryInMemory.ts
+++ src/modules/rentals/repositories/in-memory/RentalsRepositoryInMemory.ts
@@ -1,17 +1,37 @@
+import { ICreateRentalDTO } from '@modules/rentals/dtos/ICreateRentalDTO';
 import { Rental } from '@modules/rentals/infra/typeorm/entities/Rental';
 import { IRentalsRepository } from '../IRentalsRepository';
 
 class RentalsRepositoryInMemory implements IRentalsRepository {
   rentals: Rental[] = [];
 
+  async create(data: ICreateRentalDTO): Promise<Rental> {
+    const { car_id, user_id, expected_return_date } = data;
+
+    const rental = new Rental();
+
+    Object.assign(rental, {
+      car_id,
+      user_id,
+      expected_return_date,
+      start_date: new Date(),
+      created_at: new Date(),
+      updated_at: new Date(),
+    });
+
+    this.rentals.push(rental);
+
+    return rental;
+  }
+
   async findOpenRentalByCar(car_id: string): Promise<Rental> {
     return this.rentals.find(
-      rental => rental.car_id === car_id && rental.end_date === null,
+      rental => rental.car_id === car_id && !rental.end_date,
     );
   }
   async findOpenRentalByUser(user_id: string): Promise<Rental> {
     return this.rentals.find(
-      rental => rental.user_id === user_id && rental.end_date === null,
+      rental => rental.user_id === user_id && !rental.end_date,
     );
   }
 }
