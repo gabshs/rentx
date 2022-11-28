9ef4d479156c6b0f70d6e85b02e75503f14d5031
Gabriel Henrique <gabriel.henrique@warren.com.br>
2022-11-28 00:33:27 -0300

    feat: adds tests to cover when user has rental active and when car is unavailable

--- src/modules/rentals/useCases/createRental/CreateRentalUseCase.spec.ts
+++ src/modules/rentals/useCases/createRental/CreateRentalUseCase.spec.ts
@@ -1,4 +1,5 @@
 import { RentalsRepositoryInMemory } from '@modules/rentals/repositories/in-memory/RentalsRepositoryInMemory';
+import { AppError } from '@shared/errors/AppError';
 import { CreateRentalUseCase } from './CreateRentalUseCase';
 
 let rentalsRepository: RentalsRepositoryInMemory;
@@ -11,10 +12,45 @@ describe('CreateRentalUseCase', () => {
   });
 
   it('should be able to create a new Rental', async () => {
+    const rental = await createRentalUseCase.execute({
+      user_id: '12345',
+      car_id: '123123',
+      expected_return_date: new Date(),
+    });
+
+    expect(rental).toHaveProperty('id');
+    expect(rental).toHaveProperty('start_date');
+  });
+
+  it('should throw error when user has other rent in progress', async () => {
     await createRentalUseCase.execute({
       user_id: '12345',
       car_id: '123123',
       expected_return_date: new Date(),
     });
+
+    const rental2 = createRentalUseCase.execute({
+      user_id: '12345',
+      car_id: '3213312',
+      expected_return_date: new Date(),
+    });
+
+    await expect(rental2).rejects.toBeInstanceOf(AppError);
+  });
+
+  it('should throw error when car is unavailabled', async () => {
+    await createRentalUseCase.execute({
+      user_id: '321321',
+      car_id: '123123',
+      expected_return_date: new Date(),
+    });
+
+    const rental2 = createRentalUseCase.execute({
+      user_id: '12345',
+      car_id: '123123',
+      expected_return_date: new Date(),
+    });
+
+    await expect(rental2).rejects.toBeInstanceOf(AppError);
   });
 });
